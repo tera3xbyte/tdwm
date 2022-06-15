3caca4498eac69926ec6b50b896696f1fe6e13f4
Nathan Thirza <teraa3xbyte@yahoo.com>
2022-06-15 13:10:23 +0800

    added fullgaps patch

--- dwm/config.h
+++ dwm/config.h
@@ -2,9 +2,10 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int snap      = 10;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "BlexMono NF:size=13" };
 static const char dmenufont[]       = "BlexMono NF:size=13";
 static const char col_gray1[]       = "#1d2021";
@@ -39,9 +40,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "TLE",      tile },    /* first entry is default */
-	{ "FLT",      NULL },    /* no layout function means floating behavior */
-	{ "MON",      monocle },
+	{ "TILE",      tile },    /* first entry is default */
+	{ "FLOAT",      NULL },    /* no layout function means floating behavior */
+	{ "MONO",      monocle },
 };
 
 /* key definitions */
@@ -58,16 +59,17 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 // static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-// dmenu_run -X 275 -Y 150 -W 860 -h 40 -l 10 -g 3 -bw 15 -fn "BlexMono NF:style=italic"
+// dmenu_r
+// un -X 275 -Y 150 -W 860 -h 40 -l 10 -g 3 -bw 15 -fn "BlexMono NF:style=italic"
 static const char *dmenucmd[] = { "dmenu_run", 
-  "-X", "275", 
-  "-Y", "150",
-  "-W", "860",
+  "-X", "450", 
+  "-Y", "200",
+  "-W", "500",
   "-h", "40",
-  "-l", "10",
-  "-g", "3",
+  "-l", "7",
+  "-g", "2",
   "-bw", "15",
-  "-fn", "BlexMono NF",
+  "-fn", "BlexMono NF:style=italic",
   NULL };
 static const char *termcmd[]  = { "xterm", NULL };
 
@@ -102,8 +104,11 @@ static Key keys[] = {
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
 	{ MODKEY|ShiftMask,             XK_m,      quit,           {0} },
-  { MODKEY|ShiftMask,             XK_minus,    spawn,        SHCMD("pamixer --allow-boost -d 5") },
-  { MODKEY|ShiftMask,             XK_equal,    spawn,        SHCMD("pamixer --allow-boost -i 5") },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+  { MODKEY|ShiftMask,             XK_minus,    spawn,        SHCMD("pamixer --allow-boost -d 5 && notifyvol") },
+  { MODKEY|ShiftMask,             XK_equal,    spawn,        SHCMD("pamixer --allow-boost -i 5 && notifyvol") },
   { MODKEY,                       XK_r,        spawn,        SHCMD("dunstctl context") },
   { MODKEY|ShiftMask,             XK_s,        spawn,        SHCMD("scrot --select -z -b -e 'ss2 $f'") },
   { MODKEY|ShiftMask,             XK_s,        spawn,        SHCMD("scrot --select -z -b -e 'ss2 $f'") },
